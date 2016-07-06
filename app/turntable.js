f248b1006229c67ced1e83f98797e0450efee36f
Andrey Rubayko <tograilx@gmail.com>
2016-07-06 17:40:21 +0300

    Basic animations

--- app/turntable.js
+++ app/turntable.js
@@ -1,3 +1,39 @@
-/**
- * Created by Andrew on 06.07.2016.
- */
+var app = angular.module('tt', []);
+
+app.directive("turntable", [ '$interval', function($interval){
+    function link(scope, element, attrs) {
+        console.info('element: ' + element);
+        var ctx = element[0].getContext('2d');
+        ctx.font = "30px Arial";
+        ctx.fillRect(10, 20, 30, 30);
+
+        var x = 0;
+
+
+        $interval(rotate, 1000);
+
+        function rotate() {
+            ctx.clearRect(10+x, 20, 30, 30);
+
+            x+=1;
+
+            if(x > 100)
+                x = 0;
+
+            ctx.beginPath();
+            ctx.fillRect(10+x, 20, 30, 30);
+
+            //ctx.rotate(Math.PI / 4);
+
+            requestAnimationFrame(rotate)
+        }
+    }
+
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: true,
+        link: link,
+        template: '<canvas id="turntable"></canvas>'
+    };
+}]);
