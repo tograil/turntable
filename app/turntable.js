1de2caa9ea2979c89cfc7d1665ecbb1ae4520a2b
Andrey Rubayko <tograilx@gmail.com>
2016-07-06 18:35:34 +0300

    anumation

--- app/turntable.js
+++ app/turntable.js
@@ -7,25 +7,68 @@ app.directive("turntable", [ '$interval', function($interval){
         ctx.font = "30px Arial";
         ctx.fillRect(10, 20, 30, 30);
 
-        var x = 0;
 
 
-        $interval(rotate, 1000);
+        var x = 10;
+        var y = 10;
 
-        function rotate() {
-            ctx.clearRect(10+x, 20, 30, 30);
+        initHandlers(element[0]);
+        animate();
 
-            x+=1;
+        function animate() {
 
-            if(x > 100)
-                x = 0;
+            if(mousepressed)
+                ctx.fillStyle="#FF0000";
+            else
+                ctx.fillStyle="#000000";
+
+            ctx.clearRect(x, y, 30, 30);
 
             ctx.beginPath();
-            ctx.fillRect(10+x, 20, 30, 30);
+            ctx.fillRect(x, y, 30, 30);
 
             //ctx.rotate(Math.PI / 4);
 
-            requestAnimationFrame(rotate)
+
+        }
+
+        var mousepressed = false;
+
+        function initHandlers(ctx) {
+            ctx.addEventListener("mousedown", function (event) {
+
+                mousepressed = true;
+                animate();
+                requestAnimationFrame(animate);
+            });
+
+            ctx.addEventListener("mouseup", function (event) {
+                mousepressed =  false;
+                animate();
+                requestAnimationFrame(animate);
+            });
+
+            ctx.addEventListener("mousemove", function (event) {
+
+                if(mousepressed)
+                {
+                    ctx.getContext('2d').clearRect(x, y, 30, 30);
+
+                    if(event.offsetX) {
+                        x = event.offsetX-15;
+                        y = event.offsetY-15;
+                    }
+                    else if(event.layerX) {
+                        x = event.layerX-15;
+                        y = event.layerY-15;
+                    }
+
+                }
+
+                animate();
+                requestAnimationFrame(animate);
+
+            });
         }
     }
 
