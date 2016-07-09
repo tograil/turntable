c6595d9e01f1be06a87f2be72144c422eae4f969
Andrey Rubayko <tograilx@gmail.com>
2016-07-09 18:14:31 +0300

    Konva and basic animation added

--- app/turntable.js
+++ app/turntable.js
@@ -2,80 +2,116 @@ var app = angular.module('tt', []);
 
 app.directive("turntable", [ '$interval', function($interval){
     function link(scope, element, attrs) {
-        console.info('element: ' + element);
-        var ctx = element[0].getContext('2d');
-        ctx.font = "30px Arial";
+        var stage = new Konva.Stage({
+            container: element[0],   // id of container <div>
+            width: 552,
+            height: 427
+        });
+
+        var backgroundLayer = new Konva.Layer();
+        var machineLayer = new Konva.Layer();
+        var discLayer = new Konva.Layer();
+        var controlLayer = new Konva.Layer();
+
+
 
         var backgroundImage = new Image();
+        backgroundImage.onload = function () {
+            var background = new Konva.Image({
+                x:0,
+                y:0,
+                image: backgroundImage,
+                width: 552,
+                height: 427
+            });
+
+            backgroundLayer.add(background);
+
+        };
         backgroundImage.src = 'app/img/background.jpg';
 
         var machineImage = new Image();
-        backgroundImage.src = 'app/img/machine.jpg';
-
-
-
-        var x = 10;
-        var y = 10;
-
-        initHandlers(element[0]);
-        animate();
-
-        function animate() {
-            ctx.drawImage(backgroundImage, 0, 0);
-
-            if(mousepressed)
-                ctx.fillStyle="#FF0000";
-            else
-                ctx.fillStyle="#000000";
-
-            ctx.clearRect(x, y, 30, 30);
-
-            ctx.beginPath();
-            ctx.fillRect(x, y, 30, 30);
-
-            //ctx.rotate(Math.PI / 4);
-
-
-        }
-
-        var mousepressed = false;
-
-        function initHandlers(ctx) {
-            ctx.addEventListener("mousedown", function (event) {
-
-                mousepressed = true;
-                animate();
-                requestAnimationFrame(animate);
+        machineImage.onload = function () {
+            var machine = new Konva.Image({
+                x:0,
+                y:0,
+                image: machineImage,
+                width: 552,
+                height: 427
             });
 
-            ctx.addEventListener("mouseup", function (event) {
-                mousepressed =  false;
-                animate();
-                requestAnimationFrame(animate);
-            });
+            machineLayer.add(machine);
 
-            ctx.addEventListener("mousemove", function (event) {
+        };
 
-                if(mousepressed)
-                {
-                    ctx.getContext('2d').clearRect(x, y, 30, 30);
+        machineImage.src = 'app/img/machine.png';
 
-                    if(event.offsetX) {
-                        x = event.offsetX-15;
-                        y = event.offsetY-15;
-                    }
-                    else if(event.layerX) {
-                        x = event.layerX-15;
-                        y = event.layerY-15;
-                    }
-
+        var diskImage = new Image();
+        diskImage.onload = function () {
+            var disc = new Konva.Image({
+                x:233,
+                y:213,
+                image: diskImage,
+                width: 311,
+                height: 311,
+                offset: {
+                    x: 155,
+                    y: 155
                 }
-
-                animate();
-                requestAnimationFrame(animate);
-
             });
-        }
+
+            discLayer.add(disc);
+
+
+            // one revolution per 4 seconds
+            var angularSpeed = 90;
+            var anim = new Konva.Animation(function(frame) {
+                var angleDiff = frame.timeDiff * angularSpeed / 1000;
+                disc.rotate(angleDiff);
+            }, discLayer);
+
+            anim.start();
+        };
+
+        diskImage.src = 'app/img/disc.png';
+
+        var controlImage = new Image();
+        controlImage.onload = function () {
+            var control = new Konva.Image({
+                x:430,
+                y:125,
+                image: controlImage,
+                width: 110,
+                height: 310,
+                offset: {
+                    x: 55,
+                    y: 65
+                }
+            });
+
+            control.on('mousedown', function () {
+                alert('hello');
+            });
+
+            controlLayer.add(control);
+
+
+            // one revolution per 4 seconds
+            var angularSpeed = 90;
+            var animOne = new Konva.Animation(function(frame) {
+                var angleDiff = frame.timeDiff * angularSpeed / 1000;
+                control.rotate(angleDiff);
+            }, discLayer);
+        };
+
+        controlImage.src = 'app/img/control.png';
+
+
+        stage.add(backgroundLayer);
+        stage.add(machineLayer);
+        stage.add(discLayer);
+        stage.add(controlLayer);
+
     }
 
     return {
@@ -83,6 +119,6 @@ app.directive("turntable", [ '$interval', function($interval){
         replace: true,
         scope: true,
         link: link,
-        template: '<canvas id="turntable"></canvas>'
+        template: '<div></div>'
     };
 }]);
